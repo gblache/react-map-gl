4e713b3aabcc9dce3afce1bcf26fa313bc7c37ac
Graham Blache <graham.blache@gmail.com>
2016-10-03 15:18:16 -0400

    Use pre-built mapboxgl.

--- src/map.react.js
+++ src/map.react.js
@@ -21,7 +21,7 @@ import React, {PropTypes, Component} from 'react';
 import autobind from 'autobind-decorator';
 import pureRender from 'pure-render-decorator';
 
-import mapboxgl from 'mapbox-gl';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
 import {select} from 'd3-selection';
 import Immutable from 'immutable';
 import assert from 'assert';
